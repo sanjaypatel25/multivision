6006bed39ce59928684ffa092afef259fe41187f
Patel, Sanjay <java.sanjay@gmail.com>
2014-11-06 16:47:49 -0500

    Third commit
    Third commit

--- server.js
+++ server.js
@@ -1,14 +1,40 @@
-var express = require('express');
+var express = require('express'),
+	stylus = require('stylus'),
+	logger = require('morgan'),
+	bodyParser = require('body-parser'),
+	mongoose = require('mongoose');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
 
+function compile (str, path) {
+	return stylus(str).set('filename', path);
+}
+
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
 
+app.use(logger('dev'));
+app.use(bodyParser());
+app.use(stylus.middleware(
+	{
+		src: __dirname + '/public',
+		compile: compile
+	}
+));
+
+app.get('/partials/:partialPath', function(request, response) {
+	response.render('partials/' + request.params.partialPath);
+});
+
+//serve all files from /public directory
+app.use(express.static(__dirname + '/public'));
+
 app.get('*', function(request, response) {
-	response.render('index');
+	response.render('index', {
+		mongoMessage: mongoMessage
+	});
 });
 
 
@@ -16,3 +42,24 @@ var port = 3030;
 app.listen(port);
 
 console.log('Listening on port ' + port + '...');
+
+mongoose.connect('mongodb://127.0.0.1/multivision');
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error ...'));
+
+db.once('open', function callback() {
+	console.log('multivision db opened');
+});
+
+var messageSchema = mongoose.Schema({message: String});
+var Message = mongoose.model('Message', messageSchema);
+var mongoMessage;
+Message.findOne().exec(function(err, messageDoc){
+	mongoMessage = messageDoc.message;
+});
+
+
+
+
+
